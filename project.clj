ea334fdff70405a0da4bbb55806f3763288042e0
Matthew Daley <matt.daley@blueyonder.co.uk>
2015-06-24 17:12:06 +0100

    Working plugin.

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject lein-kinesis "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "lein plugin that wraps 'kinesalite' a local implementation of Amazon Kinesis."
+  :url "http://github.com/mdaley/lein-kinesis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :dependencies [[me.raynes/conch "0.8.0"]]
+  :scm {:name "git"
+        :url "https://github.com/mdaley/lein-kinesis"}
+  :eval-in-leiningen true
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
+
+  :kinesis {:ssl false
+            :port 8083})
--- src/leiningen/kinesis.clj
+++ src/leiningen/kinesis.clj
@@ -1,6 +1,52 @@
-(ns leiningen.kinesis)
+(ns leiningen.kinesis
+  (:require [leiningen.core.main :as main]
+            [me.raynes.conch :refer [programs with-programs let-programs] :as sh]))
+
+(programs which)
+(programs npm)
+(programs kinesalite)
+
+(defn- installed?
+  "Returns true if `exe` is a program that exists on the path."
+  [exe]
+  (not (clojure.string/blank? (which exe {:throw false}))))
+
+(defn- run-kinesalite
+  "Run the kinesalite process as a background process."
+  [port ssl]
+  (kinesalite "--port" port "--ssl" ssl {:background true :verbose true}))
+
+(defn- config-value
+  "Get a value from project config or, optionally, use a default value."
+  [project k & [default]]
+  (get (project :memcached) k default))
+
+(defn- prerequisites-missing?
+  "node and npm need to be installed first."
+  []
+  (not (and (installed? "node")
+            (installed? "npm"))))
+
+(defn- install-kinesalite-if-necessary
+  []
+  (when (not (installed? "kinesalite"))
+    (println "lein-kinesis: installing kinesalite")
+    (npm "install" "-g" "kinesalite")))
 
 (defn kinesis
-  "I don't do a lot."
+  "Run kinesalite in memory."
   [project & args]
-  (println "Hi!"))
+  (if (prerequisites-missing?)
+    (println "lein-kinesis: cannot execute as node and npm are not installed. Please install them and try again!")
+    (let [port (config-value project :port 8083)
+          ssl (config-value project :ssl false)]
+      (install-kinesalite-if-necessary)
+      (println (str "lein-kinesis: starting in-memory kinesalite instance on port " port "."))
+      (when ssl
+        (println "lein-kinesis: ssl enabled."))
+      (let [kinesis-server (run-kinesalite port ssl)]
+        (if (seq args)
+          (try
+            (main/apply-task (first args) project (rest args))
+            (finally (future-cancel kinesis-server)))
+          (while true (Thread/sleep 5000)))))))
